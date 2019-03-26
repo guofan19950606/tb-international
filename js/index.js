9cc9351fce47dd187d54e0c940cef590558e59b6
Your Name <1091275793@qq.com>
2019-03-26 18:42:08 +0800

    修改过后的天猫国际

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ var mySwiper = new Swiper ('.swiper-container', {
     loop: true, // 循环模式选项
     autoplay: {
     delay: 1000,
-    
+    disableOnInteraction: false,
     },
     // 如果需要分页器
     pagination: {
@@ -22,3 +22,40 @@ var mySwiper = new Swiper ('.swiper-container', {
       el: '.swiper-scrollbar',
     },
   })        
+$(function(){
+  $(".inter-swiper").scroll(function(event){
+      let temp=$(".inter-swiper").scrollLeft();
+      if(Math.ceil(temp)>0){
+        $(".pagination .pagination-item:first-child").css({"background-color":"rgb(216, 210, 210)"});
+        $(".pagination .pagination-item:last-child").css({"background-color":"rgb(180, 52, 112)"});
+      }else{
+        $(".pagination .pagination-item:first-child").css({"background-color":"rgb(180, 52, 112)"});
+        $(".pagination .pagination-item:last-child").css({"background-color":"rgb(216, 210, 210)"});
+      }
+  });
+});
+$(function(){
+  $(".grail").scroll(function(event){
+      let temp=$(".grail").scrollTop();
+      console.log(temp);
+      if(Math.ceil(temp)>0){
+          $(".tb-international-header").css({"background":"white","transition":"all 1s"});
+          $(".tmgjicon i").css({"color":"#8146EC"});
+          $(".tb-internation-header-right a i").css({"color":"black"," font-weight":"700"});
+          $(".tb-international-header-search").css({"background":"#F7F7F7"});
+      }else{
+        $(".tb-international-header").css({"background":"transparent","transition":"all 1s"});
+        $(".tmgjicon i").css({"color":"white"});
+        $(".tb-internation-header-right a i").css({"color":"white"," font-weight":"100"});
+        $(".tb-international-header-search").css({"background":"white"});
+      }
+  });
+});
+$(function(){
+  $(".tb-international-footer a").click(function(){
+    if (event.currentTarget==this) {
+      $(".tb-international-footer a").css({"color":"black"});
+      $(this).css({"color":"#8146ec"});
+    }
+  })
+})
