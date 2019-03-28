8ab776fd22601b1fada2ca95a026068f1a364fc5
Your Name <1091275793@qq.com>
2019-03-28 19:55:25 +0800

    这次添加了商品的动态效果

--- js/goodsDetil.js
+++ js/goodsDetil.js
@@ -1,7 +1,31 @@
-var mySwiper = new Swiper ('.swiper-container', {
+  var mySwiper = new Swiper ('.swiper-container', {
+  direction: 'horizontal', // 垂直切换选项
+  loop: true, // 循环模式选项
+
+  // 如果需要分页器
+  pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+      //type: 'fraction',
+      //type : 'progressbar',
+      //type : 'custom',
+    },
+
+
+  // 如果需要前进后退按钮
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  // 如果需要滚动条
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  })
+  var mySwiper = new Swiper ('.swiper-containerone', {
     direction: 'horizontal', // 垂直切换选项
     loop: true, // 循环模式选项
-    
+    autoplay:true,
     // 如果需要分页器
     pagination: {
         el: '.swiper-pagination',
@@ -26,7 +50,6 @@ var mySwiper = new Swiper ('.swiper-container', {
   $(function(){
     $(".grail").scroll(function(event){
         let temp=$(".grail").scrollTop();
-      console.log(temp);
         if(Math.ceil(temp)>44){
             $(".mui-flex").css({"background":"white"});
             $(".header-nav").css({"opacity":"1"});
@@ -39,4 +62,39 @@ var mySwiper = new Swiper ('.swiper-container', {
           $(".icon-link i").css({"color":"white"});
         }
     });
+  });
+  $(function(){
+    $(".grail").scroll(function(event){
+        let temp=$(".grail").scrollTop();
+        if(Math.ceil(temp)<900){
+          $(".header-nav li:nth-of-type(1)").addClass("active");
+          $(".header-nav li:nth-of-type(2)").removeClass("active");
+          $(".header-nav li:nth-of-type(3)").removeClass("active");
+        }else if(Math.ceil(temp)>=900&&Math.ceil(temp)<1600){
+          $(".header-nav li:nth-of-type(1)").removeClass("active");
+          $(".header-nav li:nth-of-type(2)").addClass("active");
+          $(".header-nav li:nth-of-type(3)").removeClass("active");
+        }else if(Math.ceil(temp)>1600){
+          $(".header-nav li:nth-of-type(1)").removeClass("active");
+          $(".header-nav li:nth-of-type(2)").removeClass("active");
+          $(".header-nav li:nth-of-type(3)").addClass("active");
+        }
+    });
+  });
+  $(function(){
+    $(".header-nav li").click(function(){
+      if(event.currentTarget==this){
+        $(".header-nav li").removeClass("active");
+        $(this).addClass("active");
+      }
+    })
+  })
+  $(".header-nav li:nth-of-type(1)").click(function(){
+    let temp=$(".grail").scrollTop(0);
+  });
+  $(".header-nav li:nth-of-type(2)").click(function(){
+    let temp=$(".grail").scrollTop(900);;
+  });
+  $(".header-nav li:nth-of-type(3)").click(function(){
+    let temp=$(".grail").scrollTop(1600);
   });
